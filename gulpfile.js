8f90fe19ce82e3188031537b86a671fe0da9c268
Greg Quan <gquan@adap.tv>
2019-03-16 13:00:51 -0700

    move last names into the same cell

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,8 @@ const htmlmin = require('gulp-htmlmin');
 gulp.task('minify', () => {
   return gulp.src('src/*.html')
     .pipe(htmlmin({
-    	collapseWhitespace: true,
-    	minifyCSS: true,
-        minifyJS: true,
+        collapseWhitespace: false,
+        minifyCSS: false,
         removeComments: true
     }))
     .pipe(gulp.dest('dist'));
